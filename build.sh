24dc9fb21016e81ca402c2bebaf04b2f725b8a19
Chris Richardson <chris@bpi.cam.ac.uk>
2020-06-12 12:34:16 +0100

    Get working simple example

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-gcc -c -I`python3 -c 'import ffcx.codegeneration ; print(ffcx.codegeneration.get_include_path())'` poisson.c
-g++ -c -I`python3 -c 'import ffcx.codegeneration ; print(ffcx.codegeneration.get_include_path())'` main.cpp
+ffcx poisson.ufl
+EIGEN=/usr/include/eigen3
+UFC=`python3 -c 'import ffcx.codegeneration ; print(ffcx.codegeneration.get_include_path())'`
+gcc -c -I$UFC -I$EIGEN poisson.c
+g++ -c -I$UFC -I$EIGEN main.cpp
 g++ -o rhsass main.o poisson.o
--- main.cpp
+++ main.cpp
@@ -7,11 +7,56 @@
 // dofmap
 int main()
 {
-  int npoints = 100;
+  ufc_form* L = create_form_poisson_L();
+  ufc_integral* integral = L->create_cell_integral(-1);
+  auto kernel = integral->tabulate_tensor;
 
+  int nelem = 1000;
+  int nelem_dofs = 3; // For P1 Poisson on triangle
+
+  // Create a dummy dofmap
+  Eigen::Array<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> dofmap(
+      nelem, nelem_dofs);
+  for (int i = 0; i < nelem; ++i)
+  {
+    for (int j = 0; j < nelem_dofs; ++j)
+      dofmap(i, j) = i * 2 + j;
+  }
+  int idx = dofmap.maxCoeff();
+
+  // For a P1 problem, the number of geometry points is the same as the number
+  // of dofs
+  int npoints = idx + 1;
+  std::cout << "npoints = " << npoints << "\n";
   Eigen::Array<double, Eigen::Dynamic, 3, Eigen::RowMajor> geometry
       = Eigen::Array<double, Eigen::Dynamic, 3, Eigen::RowMajor>::Random(
           npoints, 3);
 
-  std::cout << geometry << "\n";
+  // Global RHS vector
+  Eigen::VectorXd global_vector(npoints);
+  global_vector.setZero();
+
+  // Assembly loop
+  // Coefficient (RHS source term) - constant on each cell
+  Eigen::Array<double, 3, 1> w = {1.0, 2.0, 3.0};
+  // Element local RHS
+  Eigen::Array<double, 3, 1> b;
+  // Element local geometry
+  Eigen::Array<double, 3, 3, Eigen::RowMajor> cell_geometry;
+  for (int i = 0; i < nelem; ++i)
+  {
+    // Pull out points for this cell
+    for (int j = 0; j < 3; ++j)
+      cell_geometry.row(j) = geometry.row(dofmap(i, j));
+
+    // Get local values
+    kernel(b.data(), w.data(), nullptr, cell_geometry.data(), nullptr, nullptr,
+           0);
+
+    // Fill global vector
+    for (int j = 0; j < 3; ++j)
+      global_vector[dofmap(i, j)] += b[j];
+  }
+
+  std::cout << "Vector norm = " << global_vector.norm() << "\n";
 }
