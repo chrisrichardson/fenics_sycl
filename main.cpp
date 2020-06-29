815ce131af17c663a116461ed76aed602594aee7
Chris Richardson <chris@bpi.cam.ac.uk>
2020-06-29 14:29:32 +0100

    Use dolfinx assembler

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@
 #include <numeric>
 
 #include "assemble_rhs.h"
-#include "dolfin_interface.h"
 #include "poisson.h"
 
 void exception_handler(sycl::exception_list exceptions)
@@ -92,8 +91,8 @@ int main(int argc, char* argv[])
   auto V = fem::create_functionspace(create_functionspace_form_poisson_a, "u",
                                      mesh);
   std::shared_ptr<fem::Form> L = fem::create_form(create_form_poisson_L, {V});
-  
- const graph::AdjacencyList<std::int32_t>& v_dofmap = V->dofmap()->list();
+
+  const graph::AdjacencyList<std::int32_t>& v_dofmap = V->dofmap()->list();
   // Create permuted dofmap for insertion into array
   const graph::AdjacencyList<std::int32_t> flat_dm
       = create_flat_index(v_dofmap);
@@ -113,7 +112,7 @@ int main(int argc, char* argv[])
   L->set_coefficients({{"f", f}});
 
   Eigen::Array<PetscScalar, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
-    coeff = fem::pack_coefficients(*L);
+      coeff = fem::pack_coefficients(*L);
 
   int nelem = function_dm.rows();
   int nelem_dofs = function_dm.cols();
@@ -132,81 +131,58 @@ int main(int argc, char* argv[])
 
   auto timer_start = std::chrono::system_clock::now();
 
-  // Device memory to accumulate assembly entries before summing
-  cl::sycl::buffer<double, 1> ac_buf(
-      cl::sycl::range<1>{(std::size_t)(nelem * nelem_dofs)});
-  {
-    cl::sycl::buffer<int, 2> coord_dm_buf(
-        coord_dm.data(),
-        {(std::size_t)coord_dm.rows(), (std::size_t)coord_dm.cols()});
-
-    cl::sycl::buffer<double, 2> geom_buf(geometry.data(),
-                                         {(std::size_t)npoints, 3});
-
-    cl::sycl::buffer<double, 2> coeff_buf(
-        coeff.data(), {(std::size_t)coeff.rows(), (std::size_t)coeff.cols()});
-
-    cl::sycl::buffer<int, 2> fi_buf(
-        flat_dm.array().data(), {(std::size_t)nelem, (std::size_t)nelem_dofs});
-
-    assemble_rhs(queue, ac_buf, geom_buf, coord_dm_buf, coeff_buf, fi_buf);
-  }
-
   // Global RHS vector (into which accum_buf will be summed)
   Eigen::VectorXd global_vector(ndofs);
-
-  // Second kernel to accumulate RHS for each dof
   {
-    cl::sycl::buffer<double, 1> gv_buf(global_vector.data(),
-                                       global_vector.size());
-    cl::sycl::buffer<int, 1> off_buf(flat_dm.offsets().data(),
-                                     flat_dm.offsets().size());
-    accumulate_rhs(queue, ac_buf, gv_buf, off_buf);
+    // Device memory to accumulate assembly entries before summing
+    cl::sycl::buffer<double, 1> ac_buf(
+        cl::sycl::range<1>{(std::size_t)(nelem * nelem_dofs)});
+    {
+      cl::sycl::buffer<int, 2> coord_dm_buf(
+          coord_dm.data(),
+          {(std::size_t)coord_dm.rows(), (std::size_t)coord_dm.cols()});
+
+      cl::sycl::buffer<double, 2> geom_buf(geometry.data(),
+                                           {(std::size_t)npoints, 3});
+
+      cl::sycl::buffer<double, 2> coeff_buf(
+          coeff.data(), {(std::size_t)coeff.rows(), (std::size_t)coeff.cols()});
+
+      cl::sycl::buffer<int, 2> fi_buf(
+          flat_dm.array().data(),
+          {(std::size_t)nelem, (std::size_t)nelem_dofs});
+
+      assemble_rhs(queue, ac_buf, geom_buf, coord_dm_buf, coeff_buf, fi_buf);
+    }
+
+    // Second kernel to accumulate RHS for each dof
+    {
+      cl::sycl::buffer<double, 1> gv_buf(global_vector.data(),
+                                         global_vector.size());
+      cl::sycl::buffer<int, 1> off_buf(flat_dm.offsets().data(),
+                                       flat_dm.offsets().size());
+      accumulate_rhs(queue, ac_buf, gv_buf, off_buf);
+    }
   }
 
   auto timer_end = std::chrono::system_clock::now();
   std::chrono::duration<double> dt = (timer_end - timer_start);
   std::cout << "gpu=" << dt.count() << "s.\n";
-
-  timer_start = std::chrono::system_clock::now();
+  std::cout << "Vector norm = " << std::setprecision(20) << global_vector.norm()
+            << "\n";
 
   // Comparison CPU code below
   //--------------------------
+  timer_start = std::chrono::system_clock::now();
 
-  Eigen::VectorXd global_vector2(ndofs);
-  global_vector2.setZero();
-
-  auto form = create_form_poisson_L();
-  auto integral = form->create_cell_integral(-1);
-  auto tabulate_L = integral->tabulate_tensor;
-
-  // Element local RHS
-  Eigen::Array<double, 3, 1> b;
-  // Element local geometry
-  Eigen::Array<double, 3, 2, Eigen::RowMajor> cell_geometry;
-
-  for (int i = 0; i < nelem; ++i)
-  {
-    // Pull out points for this cell
-    for (int j = 0; j < 3; ++j)
-      cell_geometry.row(j) = geometry.row(coord_dm(i, j)).head(2);
-
-    b.setZero();
-    // Get local values
-    tabulate_L(b.data(), coeff.row(i).data(), nullptr, cell_geometry.data(), nullptr,
-               nullptr, 0);
-
-    // Fill global vector
-    for (int j = 0; j < nelem_dofs; ++j)
-      global_vector2[function_dm(i, j)] += b[j];
-  }
+  la::PETScVector u(*L->function_space(0)->dofmap()->index_map);
+  VecSet(u.vec(), 0);
+  dolfinx::fem::assemble_vector(u.vec(), *L);
+  double norm;
+  VecNorm(u.vec(), NORM_2, &norm);
 
   timer_end = std::chrono::system_clock::now();
   dt = (timer_end - timer_start);
   std::cout << "cpu=" << dt.count() << "s.\n";
-
-  std::cout << "Vector norm = " << std::setprecision(20) << global_vector.norm()
-            << "\n";
-  std::cout << "Vector norm 2 = " << std::setprecision(20)
-            << global_vector2.norm() << "\n";
+  std::cout << "u.norm = " << std::setprecision(20) << norm << "\n";
 }
